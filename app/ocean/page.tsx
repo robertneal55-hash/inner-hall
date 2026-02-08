26433f002757fadae6206c227f008f1846ccc3a6
robertneal55-hash <robertneal55@gmail.com>
2026-02-07 20:32:05 -0500

    Create page.tsx

---
+++ app/ocean/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ListeningChamber() {
+  return (
+    <main style={{ padding: 40, color: "white" }}>
+      <h1>Listening Chamber</h1>
+      <p>You made it.</p>
+    </main>
+  );
+}
