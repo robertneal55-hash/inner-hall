c9f833bc5933bb47343dbe1f35c4fc7bd28a921d
robertneal55-hash <robertneal55@gmail.com>
2026-02-15 12:25:29 -0500

    Update ListeningChamber.tsx

--- components/ListeningChamber.tsx
+++ components/ListeningChamber.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-interface JournalSpaceProps {
+type Props = {
   onBack: () => void;
 }
 
-export default function JournalSpace({ onBack }: JournalSpaceProps) {
-}
-
-  return (
+export default function ListeningChamber({ onBack }: Props) {
+ return (
     <div style={{ padding: 24 }}>
       <h1>Listening Chamber</h1>
       <p>This is a placeholder component so the build can compile.</p>
-      <button onClick={() => onExit?.()} style={{ marginTop: 12 }}>
-        Back
-      </button>
+    <button onClick={onBack} style={{ marginTop: 12 }}>
+  Back
+</button>
     </div>
   );
 }
